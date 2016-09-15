b46871d0fa7354eb94bd8407e9edccde48c1122b
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2016-09-15 13:07:09 +0700

    change background to black color

--- atom-one-dark-theme.el
+++ atom-one-dark-theme.el
@@ -18,7 +18,7 @@
 (defvar atom-one-dark-colors-alist
   '(("atom-one-dark-accent"   . "#528BFF")
     ("atom-one-dark-fg"       . "#ABB2BF")
-    ("atom-one-dark-bg"       . "#282C34")
+    ("atom-one-dark-bg"       . "#000000")
     ("atom-one-dark-bg-1"     . "#121417")
     ("atom-one-dark-bg-hl"    . "#2F343D")
     ("atom-one-dark-mono-1"   . "#ABB2BF")
